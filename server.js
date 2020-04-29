f8cf15bbe54f7af1763eee305c63de125e070d78
Ryan Harris <harris.ucla@gmail.com>
2020-04-28 19:21:56 -0700

    Fix connection error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public", { extensions: "html" }));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout,", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
