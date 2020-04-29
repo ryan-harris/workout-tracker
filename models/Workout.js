9533bfce447b80feee5bfbe1f876abdbfd5afafc
Ryan Harris <harris.ucla@gmail.com>
2020-04-28 19:22:17 -0700

    Add virtual for totalDuration to workout model

--- models/Workout.js
+++ models/Workout.js
@@ -2,32 +2,46 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
-  },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
 
-  exercises: [
-    {
-      type: {
-        type: String,
-        required: "Exercise type is required"
-      },
-      name: {
-        type: String,
-        required: "Exercise name is required"
-      },
-      duration: {
-        type: Number,
-        required: "Exercise duration is required"
-      },
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      distance: Number
+    exercises: [
+      {
+        type: {
+          type: String,
+          required: "Exercise type is required"
+        },
+        name: {
+          type: String,
+          required: "Exercise name is required"
+        },
+        duration: {
+          type: Number,
+          required: "Exercise duration is required"
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
     }
-  ]
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((acc, curr) => {
+    acc = acc + curr.duration;
+    return acc;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
