f9a83386b2968987bf9a77268f93c0f02748a937
Ryan Harris <harris.ucla@gmail.com>
2020-04-28 22:28:59 -0700

    workout range get route and sort get all workout routes

--- routes/api.js
+++ routes/api.js
@@ -28,9 +28,10 @@ router.put("/api/workouts/:id", (req, res) => {
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .then((workouts) => {
-      res.json(workouts);
+  db.Workout.find()
+    .sort({ day: 1 })
+    .then((data) => {
+      res.json(data);
     })
     .catch((err) => {
       res.json(err);
@@ -39,7 +40,15 @@ router.get("/api/workouts", (req, res) => {
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  // CODE HERE
+  db.Workout.find()
+    .sort({ day: -1 })
+    .limit(7)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 // Delete workout with id matching id in the request body.
